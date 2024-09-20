d3b789519b5facbf1171f91656488327b75636c0
Sanjana <sgopal0809@gmail.com>
2024-09-19 21:09:48 -0400

    change images paths

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,42 +4,42 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carous
 import './Portfolio.css';
 
 const images = [
-  '/images/post1.webp',
-  '/images/post2.webp',
-  '/images/post3.webp',
-  '/images/post4.webp',
-  '/images/post5.webp',
-  '/images/post6.webp',
-  '/images/post7.webp',
-  '/images/post8.webp',
-  '/images/post9.webp',
+  'my_website_portfolio/post1.webp',
+  'my_website_portfolio/post2.webp',
+  'my_website_portfolio/post3.webp',
+  'my_website_portfolio/post4.webp',
+  'my_website_portfolio/post5.webp',
+  'my_website_portfolio/post6.webp',
+  'my_website_portfolio/post7.webp',
+  'my_website_portfolio/post8.webp',
+  'my_website_portfolio/post9.webp',
   // add images here
 ];
 
 const tiles = [
   {
-    image: '/images/heelhouse.png',
+    image: 'my_website_portfolio/heelhouse.png',
     title: 'Heel House',
     software: 'Swift/SwiftUI',
     description: 'UNC Chapel Hill-based roommate matching app.',
     link: 'https://github.com/caitlinestrada27/heelhouse',
   },
   {
-    image: '/images/epic.jpeg',
+    image: 'my_website_portfolio/epic.jpeg',
     title: 'Rubbish Economics',
     software: 'Figma, UserTesting',
     description: 'High-fidelity design wireframes for a volunteer & crowd-sourcing platform for street garbage clean-ups in Bangalore, India.',
     link: 'https://www.figma.com/proto/Z66UHeU8UtBM8fbU3DSgu2/Energy-Policy-Institute-at-the-University-of-Chicago---India?node-id=1027-1382&node-type=CANVAS&t=glcQixQNHE72Dqpi-1&scaling=scale-down&content-scaling=fixed&page-id=6%3A4&starting-point-node-id=1027%3A1382&show-proto-sidebar=1',
   },
   {
-    image: '/images/jungle.png',
+    image: 'my_website_portfolio/jungle.png',
     title: 'Jungle Dash',
     software: 'Java',
     description: 'A simple side-scroller game, inspired by the T-Rex game when you lose internet connection on Google Chrome.',
     link: 'http://example.com/project-three',
   },
   {
-    image: '/images/nytmini.png',
+    image: 'my_website_portfolio/nytmini.png',
     title: 'NYT Mini App',
     software: 'Swift/Swift UI',
     description: 'Recreation of the New York Times Mini Crossword game.',
