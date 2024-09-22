ce1dc351acf371f54e8b6135b575bab760d0350b
Sanjana <sgopal0809@gmail.com>
2024-09-22 13:44:22 -0400

    app.js changes added welcome section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <Navbar />
+      <Welcome />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
