10adeb44bbb8e1a5827908e2601be65390a109af
Sanjana <sgopal0809@gmail.com>
2024-09-22 13:47:16 -0400

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 // import Welcome from './components/Welcome';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 
 function App() {
