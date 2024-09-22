3aa561a421297d2a93b4f5f86273ffed32fefd3b
Sanjana <sgopal0809@gmail.com>
2024-09-22 13:52:27 -0400

    change order in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-// import Welcome from './components/Welcome';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
+import Welcome from './components/Welcome';
+import Experience from './components/Experience';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      {/* <Welcome /> */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <div id="about">
+        <Welcome />
+      </div>
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="portfolio">
+        <Portfolio />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+      <Footer />
     </Router>
   );
 }
