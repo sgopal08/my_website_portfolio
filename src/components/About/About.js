6bc01b991cc9fd44958ad030a99c15aefd31215b
Sanjana <sgopal0809@gmail.com>
2026-01-03 22:43:16 -0500

    updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,7 +20,7 @@ function About() {
             </a>
           </div> */}
         <div className="profile-blurb">
-          <p>Hi, I’m Sanjana! I’m an undergraduate student at UNC Chapel Hill, double majoring in computer science and advertising. I'm passionate about technology and branding, and interested in front-end software engineering and product management.</p>
+          <p>Hi, I’m Sanjana! I’m an undergraduate student at UNC Chapel Hill, double majoring in computer science and advertising. I'm passionate about technology and media, and interested in full-stack software engineering and product development.</p>
           <p>Creativity is at the heart of everything I do– whether I’m painting landscapes or building a mobile app game. I'm also dedicated to teaching, both as a teaching assistant for the UNC CS department and as an assistant dance teacher for over 4 years.</p>
           <p>Outside of school, I love horror movies, R&B music, and exploring new cuisines. Take a look at my work and experiences below!</p>
         </div>
